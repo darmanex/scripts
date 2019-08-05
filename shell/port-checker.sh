9b0d363fd92cc6e08b6de108b80efe955127a0d3
daRm <darmanex@gmail.com>
2019-08-05 14:24:21 +0700

    Changed some params

--- shell/port-checker.sh
+++ shell/port-checker.sh
@@ -6,19 +6,39 @@
 # menentukan server, defaultnya set ke localhost
 # silakan diubah jika ingin menembak host lain
 SERVER=$HOSTNAME
+PORTDIR=/var/log/port-checker
+CURDATE=$(date)
+LOGFILE=$CURDIR/port-checker.log
+
 
 clear
-echo "Checking open port for $SERVER ..."
+echo "Checking open port for $SERVER"
+echo "Please wait a second ..."
 sleep 0
 
-# deklarasi list port yang akan di cek
+
+add_log()
+{
+    mkdir -p $PORTDIR && cd $PORTDIR && touch $LOGFILE
+}
+
+# list port yang akan di cek
 declare -a SERVICES=(
+    "80 (http)"
+    "443 (https)"
+    "4150 (nsqd)"
     "4151 (nsqd)"
     "4160 (nsqlookupd)"
     "4161 (nsqlookupd)"
     "4171 (nsqadmin)"
     "5577 (messaging)"
     "5588 (messaging)"
+    "9160 (Cassandra)"
+    "8181 (S3-Cassandra)"
+    "9200 (Elasticsearch)"
+    "9300 (Elasticsearch)"
+    "7199 (JMX)"
+    "9042 (CQWL)"
 )
 
 IFS=""
@@ -27,13 +47,20 @@ for PORT in ${SERVICES[@]};
 do
     sleep 1
     nc -z -w5 $SERVER $PORT
-    
+
     if [ "$?" -eq 0 ]; then
-        echo "[+] Server $SERVER on port $PORT - Open"
+        echo "[✔] Server $SERVER on port $PORT - Open" #>> $LOGFILE
     else
-        echo "[-] Server $SERVER on port $PORT - Closed"
+        echo "[X] Server $SERVER on port $PORT - Closed" #>> $LOGFILE
     fi
-    
+
+
 done 2>/dev/null
+
+add_log
+
+echo "Last checked at $CURDATE" >> $LOGFILE
+
+echo "Logs saved in $PORTDIR"
 
 echo "Checking port done!"
