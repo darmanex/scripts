cae584835bdde17ffd7dd40580e74c0a86581a30
darmanex <darmanex@gmail.com>
2020-06-09 12:08:15 +0700

    Add docker cleaning images and containers script

---
+++ python/docker_cleaning.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# Description:
+# this script it uses to cleanup docker images and containers on Aino servers (dev, staging, sandbox)
+# better to execute this script via cronjob
+
+import os
+
+print("Cleaning containers exited status and docker none images are proccesing..")
+os.system('sleep 2')
+
+exited_containers= os.system('docker ps -a -q -f "status=exited"')
+none_images = os.system("docker images | grep '^<none>' | awk '{print $3}'")
+
+if exited_containers == True:
+    os.system('docker rm $exited_containers')
+    print("[+] Cleanup containers that have exited status...")
+elif none_images == True:
+    os.system('docker rmi $none_images')
+    print("[+] Cleanup images that have none status...")
+else:
+    print("All done. No containers exited and none images on your system!")
